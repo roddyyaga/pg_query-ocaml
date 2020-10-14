2f35624c92563fa119e78047971d72d4381f1097
Torben Ewert <torben@ewert-online.com>
2020-10-14 20:51:48 +0200

    WIP: Replace core with cmdliner

--- bin/pg_check.ml
+++ bin/pg_check.ml
@@ -1,10 +1,14 @@
 (** A utility to parse PostgreSQL code *)
 
-open Core
+open Cmdliner
 
 let get_contents = function
-  | "-" -> In_channel.input_all In_channel.stdin
-  | filename -> In_channel.read_all filename
+  (* | "-" -> In_channel.input_all In_channel.stdin *)
+  | filename ->
+      let ic = open_in filename in
+      let contents = really_input_string ic (in_channel_length ic) in
+      close_in ic;
+      contents
 
 let do_parse files =
   let f exit_status filename =
@@ -17,18 +21,24 @@ let do_parse files =
         print_endline message;
         1
   in
-  let status = List.fold ~init:0 ~f files in
+  let status = files |> List.fold_left f 0 in
   exit status
 
-let command =
-  Command.basic ~summary:"Parses PostgreSQL"
-    ~readme:(fun () ->
-      "Given a list of files, parses each of them as PostgreSQL and prints\n\
-       either the parsetree or an error message for each. Exits with code 0\n\
-       if all files were successfully parsed and with code 1 otherwise. If no\n\
-       files are provided, reads from stdin.")
-    Command.Let_syntax.(
-      let%map_open files = anon (sequence ("filename" %: Filename.arg_type)) in
-      fun () -> match files with [] -> do_parse [ "-" ] | _ -> do_parse files)
+let info =
+  let doc = "Parses PostgreSQL" in
+  let man = [
+    `S Manpage.s_description;
+    `P "Given a list of files, parses each of them as PostgreSQL and prints\n\
+        either the parsetree or an error message for each. Exits with code 0\n\
+        if all files were successfully parsed and with code 1 otherwise."
+  ]
+  in
+  Term.info "pg_check" ~version:"0.9.4" ~doc ~exits:Term.default_exits ~man
 
-let () = Command.run ~version:"0.9.4" ~build_info:"" command
+let files = 
+  let doc = "A list of files to parse. If no files are provided, reads from stdin." in
+  Arg.(non_empty & pos_all file [] & info [] ~docv:"FILE(S)" ~doc)
+
+let cmd = Term.(const do_parse $ files)
+
+let () = Term.exit @@ Term.eval (cmd, info)
